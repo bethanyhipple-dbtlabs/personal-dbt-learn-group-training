e789d8d6f080029b42781135497f96c1d58ebb7a
Bethany Hipple <bethany.hipple@fishtownanalytics.com>
2023-05-31 17:58:17 +0000

    fix macro

--- macros/override_default_macros.sql/override_default_macros.sql
+++ macros/override_default_macros.sql/override_default_macros.sql
@@ -1,13 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {% elif target.name == 'CI'%}
+    {%- if target.name == 'CI' -%}
 
         ci_schema
+
+    {% elif custom_schema_name is none%}
+
+        {{ default_schema }}
 
     {%- else -%}
 
