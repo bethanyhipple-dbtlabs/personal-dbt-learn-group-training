e023ae009216be4816d82ac914274d981c82fe6a
Bethany Hipple <bethany.hipple@fishtownanalytics.com>
2023-05-31 17:00:49 +0000

    custom schema and alias macros

--- macros/override_default_macros.sql/override_default_macros.sql
+++ macros/override_default_macros.sql/override_default_macros.sql
@@ -15,13 +15,29 @@
 
  {% macro generate_alias_name(custom_alias_name=none, node=none) -%}
  
+    {%- if target.name == 'CI' -%}
+        {%- if custom_alias_name is none -%}
+
+            {{ env_var(DBT_CLOUD_PR_ID) }}__{{ node.name }}
+
+         {%- else -%}
+ 
+            {{ custom_alias_name | trim }}
+
+        {%- endif -%}
+
+    {%- endif -%}
+
     {%- if target.name == 'dev' -%}
+        {%- if custom_alias_name is none -%}
  
-        {{ target.schema }}_{{ node.name }}
+            {{ target.schema }}_{{ node.name }}
  
-    {%- else -%}
+        {%- else -%}
  
-        {{ node.name }}
+            {{ custom_alias_name | trim }}
+
+        {%- endif -%}
  
     {%- endif -%}
  
